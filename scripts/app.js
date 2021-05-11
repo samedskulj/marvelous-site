c5543dcd50e380844b4b9cfbe28e698ee12790e0
Samed Skulj <samed.skulj08@gmail.com>
2021-05-11 12:41:57 +0200

    Data iz API je implementirana

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,51 @@
 const btnPokreni = document.querySelector(".btn-pokreni");
-const testFetcha = async () => {
-  const res = await fetch(
-    "https://gateway.marvel.com:443/v1/public/characters?apikey=219d6c078d5b6a252a1a45d83cc19cfb"
-  );
-  const data = await res.json();
-  console.log(data);
-};
-btnPokreni.addEventListener("click", testFetcha);
-console.log("poyy");
+const btnTest = document.querySelector(".btn-test");
+const colHeroes = document.querySelector(".row-heroes");
+let podaci = [];
+class FetchApi {
+  testFetcha = async () => {
+    const res = await fetch(
+      "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=Iron&orderBy=name&limit=100&apikey=219d6c078d5b6a252a1a45d83cc19cfb"
+    );
+    podaci = await res.json();
+    console.log(podaci);
+    this.insertDiv();
+  };
+  insertDiv = () => {
+    let loadingAPI;
+    let loadingSeries;
+    podaci.data.results.map((hero) => {
+      loadingAPI = `
+      <div class = "col-12 mb-5 col-heroes" key=${hero.id}>
+        <div class="first-part-col-heroes"> 
+        <h3 id = "hero-name-id">${hero.name}</h3>
+        <div class = "img-col-heroes">
+          <img src="${hero.thumbnail.path}/portrait_uncanny.jpg"/>
+        </div> 
+       </div>   
+       <div class="second-part-col-heroes"> 
+       <h2>Description:</h2>
+       <p id = "hero-desc">${
+         hero.description || "Description is not provided for this character"
+       }</p>
+        <h2>Series:</h2>
+            <p id="series-id">
+              ${hero.series.items.map((item) => {
+                return `${item.name}`;
+              })}
+            </p>
+            <h3>Comics:</h3>
+            <p id="comics-id">
+              ${hero.comics.items.map((item) => {
+                return `${item.name}`;
+              })}
+            </p>
+       </div>
+       </div>
+      `;
+      colHeroes.innerHTML += loadingAPI;
+    });
+  };
+}
+
+const objFetchApi = new FetchApi();
